eda68eaf504d7acad19700a700c2821eb289aed3
Rhythm Saha <rs.2001.saha@gmail.com>
2023-11-03 01:43:49 +0530

    my trips

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -8,6 +8,7 @@ import useLoginModal from "@/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/types";
 import useRentModal from "@/hooks/useRentModal";
+import { useRouter } from "next/navigation";
 
 type Props = {
     currentUser?: SafeUser | null;
@@ -17,6 +18,8 @@ const UserMenu = ({ currentUser }: Props) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const rentModal = useRentModal();
+
+    const router = useRouter();
 
     const [open, setOpen] = useState(false);
 
@@ -59,11 +62,11 @@ const UserMenu = ({ currentUser }: Props) => {
                     <div className="flex flex-col cursor-pointer">
                         {currentUser ? (
                             <>
-                                <MenuItem label="My trips" onClick={() => {}} />
-                                <MenuItem label="My favorites" onClick={() => {}} />
-                                <MenuItem label="My reservations" onClick={() => {}} />
-                                <MenuItem label="My properties" onClick={() => {}} />
-                                <MenuItem label="Airbnb my home" onClick={rentModal.onOpen} />
+                                <MenuItem label="My trips" onClick={() => router.push("/trips")} />
+                                <MenuItem label="My favorites" onClick={() => router.push("/favorites")} />
+                                <MenuItem label="My reservations" onClick={() => router.push("/reservations")} />
+                                <MenuItem label="My properties" onClick={() => router.push("/properties")} />
+                                <MenuItem label="Airbnb your home" onClick={rentModal.onOpen} />
                                 <hr />
                                 <MenuItem label="Logout" onClick={() => signOut()} />
                             </>
